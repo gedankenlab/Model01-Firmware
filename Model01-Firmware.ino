bb7603c69f87b96fad49cf2c8ef84c04e291c312
Michael Richters <gedankenexperimenter@gmail.com>
2017-11-11 14:52:57 -0600

    First draft of my own keymap for Keyboardio Model01

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -117,7 +117,10 @@ enum { MACRO_VERSION_INFO,
   * the numbers 0, 1 and 2.
   */
 
-enum { QWERTY, FUNCTION, NUMPAD }; // layers
+enum { BASE, 
+       BLUE,
+       RED,
+       GREEN }; // layers
 
 /* This comment temporarily turns off astyle's indent enforcement
  *   so we can make the keymaps actually resemble the physical key layout better
@@ -126,38 +129,56 @@ enum { QWERTY, FUNCTION, NUMPAD }; // layers
 
 const Key keymaps[][ROWS][COLS] PROGMEM = {
 
-  [QWERTY] = KEYMAP_STACKED
-  (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
-   Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
-   Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
-   Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
+  [BASE] = KEYMAP_STACKED
+  (___,                   Key_1,       Key_2,  Key_3,  Key_4,  Key_5,  Key_LEDEffectNext,
+   Key_Backtick,          Key_Quote,   Key_V,  Key_Z,  Key_P,  Key_Y,  Key_Tab,
+   Key_LeftCurlyBracket,  Key_A,       Key_O,  Key_E,  Key_U,  Key_I,
+   Key_LeftBracket,       Key_Period,  Key_Q,  Key_J,  Key_K,  Key_X,  Key_Delete,
+
    Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
-   ShiftToLayer(FUNCTION),
+   LockLayer(BLUE),
 
-   M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         Key_KeypadNumLock,
-   Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
-                  Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
-   Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
+   Key_F13,  Key_6,  Key_7,  Key_8,  Key_9,  Key_0,      Key_KeypadNumLock,
+   Key_Enter,     Key_F,  Key_G,  Key_C,  Key_R,  Key_Minus,  Key_Slash,
+                  Key_D,  Key_H,  Key_T,  Key_N,  Key_S,      Key_RightCurlyBracket,
+   Key_Escape,    Key_B,  Key_M,  Key_W,  Key_L,  Key_Comma,  Key_RightBracket,
+
    Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
-   ShiftToLayer(FUNCTION)),
+   LockLayer(RED)),
 
-  [FUNCTION] =  KEYMAP_STACKED
-  (___,      Key_F1,           Key_F2,      Key_F3,     Key_F4,        Key_F5,           XXX,
+  [BLUE] =  KEYMAP_STACKED
+  (XXX,      Key_F1,           Key_F2,      Key_F3,     Key_F4,        Key_F5,           XXX,
    Key_Tab,  ___,              Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE,
    Key_Home, Key_mouseL,       Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,
    Key_End,  Key_PrintScreen,  Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  Key_mouseWarpSE,
    ___, Key_Delete, ___, ___,
    ___,
 
-   Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,          Key_F10,          Key_F11,
-   Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LeftCurlyBracket,     Key_RightCurlyBracket,    Key_LeftBracket, Key_RightBracket, Key_F12,
-                               Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,              Key_RightArrow,  ___,              ___,
-   Key_PcApplication,          Key_Mute,               Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,             Key_Backslash,    Key_Pipe,
+   XXX,  Key_F6,        Key_F7,         Key_F8,         Key_F9,          Key_F10,  Key_F11,
+   XXX,  Key_PageUp,    Key_Home,       Key_UpArrow,    Key_End,         XXX,      Key_F12,
+         Key_PageDown,  Key_LeftArrow,  Key_DownArrow,  Key_RightArrow,  ___,      ___,
+   XXX,  XXX,           XXX,            XXX,            XXX,             Key_Backslash,    Key_Pipe,
    ___, ___, Key_Enter, ___,
    ___),
 
 
-  [NUMPAD] =  KEYMAP_STACKED
+  [RED] =  KEYMAP_STACKED
+  (___, ___, ___, ___, ___, ___, ___,
+   ___, ___, ___, ___, ___, ___, ___,
+   ___, ___, ___, ___, ___, ___,
+   ___, ___, ___, ___, ___, ___, ___,
+   ___, ___, ___, ___,
+   ___,
+
+   M(MACRO_VERSION_INFO),  ___, Key_Keypad7, Key_Keypad8,   Key_Keypad9,        Key_KeypadSubtract, ___,
+   ___,                    ___, Key_Keypad4, Key_Keypad5,   Key_Keypad6,        Key_KeypadAdd,      ___,
+                           ___, Key_Keypad1, Key_Keypad2,   Key_Keypad3,        Key_Equals,         Key_Quote,
+   ___,                    ___, Key_Keypad0, Key_KeypadDot, Key_KeypadMultiply, Key_KeypadDivide,   Key_Enter,
+   ___, ___, ___, ___,
+   ___),
+
+
+  [GREEN] =  KEYMAP_STACKED
   (___, ___, ___, ___, ___, ___, ___,
    ___, ___, ___, ___, ___, ___, ___,
    ___, ___, ___, ___, ___, ___,
@@ -171,6 +192,7 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
    ___,                    ___, Key_Keypad0, Key_KeypadDot, Key_KeypadMultiply, Key_KeypadDivide,   Key_Enter,
    ___, ___, ___, ___,
    ___)
+
 };
 
 /* Re-enable astyle's indent enforcement */
@@ -312,7 +334,7 @@ void setup() {
 
   // While we hope to improve this in the future, the NumLock plugin
   // needs to be explicitly told which keymap layer is your numpad layer
-  NumLock.numPadLayer = NUMPAD;
+  NumLock.numPadLayer = RED;
 
   // We configure the AlphaSquare effect to use RED letters
   AlphaSquare.color = { 255, 0, 0 };
